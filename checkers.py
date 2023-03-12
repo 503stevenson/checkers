abb94ee8e7bd0df790139948b181b07e806dcb00
Michael Stevenson <503stevenson@gmail.com>
2023-03-12 01:54:16 -0500

    piece movements and user turns.

--- checkers.py
+++ checkers.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 
 WIDTH = 800
@@ -12,6 +13,7 @@ blue_img = pygame.image.load('figures/blue.png')
 blue_img = pygame.transform.scale(blue_img, (65, 65))
 red_img = pygame.image.load('figures/red.png')
 red_img = pygame.transform.scale(red_img, (65, 65))
+turn = 'red'
 
 class Square:
     def __init__(self, row, col, width):
@@ -76,6 +78,25 @@ class Board:
                     WIN.blit(red_img, (square.x + 16, square.y + 16))
         pygame.display.update()
 
+def changeTurn(turn):
+    if turn == 'red':
+        turn = 'blue'
+    elif turn == 'blue':
+        turn = 'red'
+    return turn
+
+def validateMove(board, selected, square):
+    if selected.piece == 'red':
+        if square.row == selected.row - 1 and (square.col == selected.col + 1 or square.col == selected.col - 1):
+            return True
+    elif selected.piece == 'blue':
+        if square.row == selected.row + 1 and (square.col == selected.col + 1 or square.col == selected.col - 1):
+            return True
+
+    elif selected.piece in ['redKing', 'blueKing']:
+        pass
+    return False
+
 def getClickedPos(pos, width):
     gap = width // 8
     x, y = pos
@@ -83,10 +104,34 @@ def getClickedPos(pos, width):
     col = x // gap
     return row, col
 
+def handleClick(board, selected, square):
+    global turn
+    #the user who's turn it is has selected one of their pieces
+    if square.piece != None and turn in square.piece:
+        #the user who's turn it is has changed their piece selection
+        if selected != None and turn in selected.piece:
+            selected.color = selected.tmpColor
+            square.color = TURQUOISE
+            selected = square
+        #the user is making an initial piece selection
+        elif selected == None:
+            square.color = TURQUOISE
+            selected = square
+        return selected
+    #the user has already selected their piece and is making a move
+    elif selected != None and square.piece == None:
+        if validateMove(board, selected, square):
+            square.piece = selected.piece
+            selected.piece = None
+            selected.color = selected.tmpColor
+            turn = changeTurn(turn)
+            return None
+    return selected
+
+
 def main():
     run = True
     board = Board(WIDTH)
-    turn = 'red'
     selected = None
 
     while run:
@@ -94,16 +139,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_BACKSPACE:
+                    sys.exit()
+
             #left click screen
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
                 row, col = getClickedPos(pos, WIDTH)
                 square = board.board[row][col]
-
-                if square.piece != None and turn in square.piece:
-                    if selected != None:
-                        selected.color = selected.tmpColor
-                    square.color = TURQUOISE
-                    selected = square
+                selected = handleClick(board, selected, square)
 
 main()
